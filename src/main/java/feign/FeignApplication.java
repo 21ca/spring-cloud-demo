4ff4ae81ba0fe82e755fa52971219ac63722939c
ahan <andy.han@activenetwork.com>
2017-08-07 14:53:41 +0800

    Add Feign demo

--- src/main/java/feign/FeignApplication.java
+++ src/main/java/feign/FeignApplication.java
@@ -3,9 +3,11 @@ package feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringCloudApplication
 @EnableFeignClients
+@EnableHystrix
 public class FeignApplication {
 	
 	public static void main(String[] args) {
